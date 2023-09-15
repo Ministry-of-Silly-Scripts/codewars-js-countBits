df9ea64b0cd790d79966d6e0f766452eead556fe
Marcio Silva <marcio.silva@ibm.com>
2023-09-15 12:00:52 -0300

    intToBin: it should return '10' to input equal to 2

--- src/countBits.js
+++ src/countBits.js
@@ -3,7 +3,17 @@ function countBits(n) {
 }
 
 function intToBin(n) {
-  return 1;
+  let bin = [];
+  let q = n;
+  let r = 0;
+  do {
+    r = q % 2;
+    q = Math.floor(q / 2);
+    bin.push(r);
+    console.log(q, r, bin);
+  } while (q != 0);
+
+  return bin.reverse().join('');
 }
 
 module.exports = { countBits, intToBin };
--- src/countBits.test.js
+++ src/countBits.test.js
@@ -8,7 +8,11 @@ describe('the countBits canary spec', () => {
 
 describe('intToBin', () => {
   it('should return 1 for input equal to 1', () => {
-    expect(intToBin(1)).toBe(1);
+    expect(intToBin(1)).toBe('1');
+  });
+
+  it('should return "10" for input equal to 2', () => {
+    expect(intToBin(2)).toBe('10');
   });
 });
 
