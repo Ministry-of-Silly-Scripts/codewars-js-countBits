545eb7b3bb4f46d3911f6503b949f10a1738262e
Marcio Silva <marcio.silva@ibm.com>
2023-09-15 13:19:09 -0300

    it should return 6 to input equal to 12345

--- src/countBits.test.js
+++ src/countBits.test.js
@@ -32,4 +32,8 @@ describe('countBits', () => {
   it('should return 2 for input equal to 10', () => {
     expect(countBits(10)).toBe(2);
   });
+
+  it('should return 6 for input equal to 12345', () => {
+    expect(countBits(12345)).toBe(6);
+  });
 });
